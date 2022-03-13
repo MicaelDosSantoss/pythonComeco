7e988eda9005a4cf5e65792f8be219fcf039cf51
knuckelstr <micaelds243@gmail.com>
2022-03-13 20:32:05 -0300

    Primeira vez usando python

---
+++ oiii.py
@@ -0,0 +1,24 @@
+
+# para fazer uma variavel no Python vc precisa apenas colocar o nome da variavel é o valor
+
+
+a = ", Quase esqueci , Olá, mundo"
+
+# Print ele mostra o resultado na tela
+
+print('Oi, primeira vez no python'+ a )
+
+# ------------------------------
+
+#  primeira  conta no Python
+
+n1 = 3
+n2 = 3
+
+print(n1 + n2)
+
+
+
+
+# ------------------------------
+
